f076b8e65605a435ef393ae266643fac65c3e6d9
Sergey Romanenko <70233726+purpln@users.noreply.github.com>
2025-10-15 12:45:58 +0300

    no message

--- Sources/Application/Application.swift
+++ Sources/Application/Application.swift
@@ -73,18 +73,31 @@ final class Application: NativeActivityDelegate {
     }
     
     func dummyRender(window: OpaquePointer?) {
-        ANativeWindow_setBuffersGeometry(window, 1, 1, HardwareBufferFormat.r8g8b8a8Unorm.rawValue)
+        let format: HardwareBufferFormat = .r8g8b8a8Unorm
         
-        var buffer = ANativeWindow_Buffer()
-        var rect = ARect()
-        
-        let result = ANativeWindow_lock(window, &buffer, &rect)
+        var result: CInt
+        result = ANativeWindow_setBuffersGeometry(window, 2, 2, format.rawValue)
         guard result == 0 else {
             android_log(priority: .error, tag: "swift", message: "renderer error: \(result)")
             return
         }
+        
+        var buffer = ANativeWindow_Buffer()
+        var rect = ARect()
+        
+        result = ANativeWindow_lock(window, &buffer, &rect)
+        guard result == 0 else {
+            android_log(priority: .error, tag: "swift", message: "renderer error: \(result)")
+            return
+        }
+        
         fill(pointer: buffer.bits, width: buffer.width, height: buffer.height)
-        ANativeWindow_unlockAndPost(window)
+        
+        result = ANativeWindow_unlockAndPost(window)
+        guard result == 0 else {
+            android_log(priority: .error, tag: "swift", message: "renderer error: \(result)")
+            return
+        }
     }
     
     func fill(pointer: UnsafeMutableRawPointer, width: CInt, height: CInt) {
@@ -97,6 +110,21 @@ final class Application: NativeActivityDelegate {
         buffer[1] = 0x00 //green
         buffer[2] = 0x00 //blue
         buffer[3] = 0x00 //alpha
+        
+        buffer[4] = 0x00
+        buffer[5] = 0xff
+        buffer[6] = 0x00
+        buffer[7] = 0x00
+        
+        buffer[8] = 0x00
+        buffer[9] = 0x00
+        buffer[10] = 0xff
+        buffer[11] = 0x00
+        
+        buffer[12] = 0xff
+        buffer[13] = 0xff
+        buffer[14] = 0xff
+        buffer[15] = 0x00
     }
 }
 
